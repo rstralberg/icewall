d954b5e15f9c5b42a1905e19dd61d108cb41ad6f
rstralberg <rstralberg@pm.me>
2023-10-07 17:57:11 +0000

    Config for project "GrafitGubben"

--- php/config.php
+++ php/config.php
@@ -8,22 +8,22 @@ const HOSTNAME = DEV_HOSTNAME;
 //  MySQL access
 const DB_HOST = 'localhost';
 const DB_USER = 'root';
-const DB_DATABASE = 'icewall';
+const DB_DATABASE = 'grafitgubben';
 
-const DB_PASSW_DEVEL = 'admin';
-const DB_PASSW_SERVER = 'admin!';
+const DB_PASSW_DEVEL = '';
+const DB_PASSW_SERVER = '';
 const DB_PASSW = DB_PASSW_DEVEL;
 
 //  Default settings
-const DEFAULT_SITENAME = 'IceWall';
+const DEFAULT_SITENAME = 'GrafitGubben';
 const DEFAULT_SITEOWNER = 'Roland Strålberg';
 const DEFAULT_LOGO = 'icons/icewall-512x512.png';
 const DEFAULT_THEME = 'Natt';
 
 //  Default user = admin
-const DEFAULT_USERNAME = 'admin';
+const DEFAULT_USERNAME = 'roland';
 const DEFAULT_FULLNAME = DEFAULT_SITEOWNER;
-const DEFAULT_EMAIL = 'admin@icewall.com';
+const DEFAULT_EMAIL = 'rstralberg@pm.me';
 const DEFAULT_PASSW = DB_PASSW_DEVEL;
 const DEFAULT_PICTURE = 'icons/icewall-512x512.png';
 
